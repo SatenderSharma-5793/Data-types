4b62d0c8864221eb49cc949589e8cea3be93d6b7
Satender Sharma <sharmasatender5793@gmail.com>
2026-09-14 21:23:30 +0530

    Add files via upload

---
+++ Single value data type.py
@@ -0,0 +1,21 @@
+>>> a=10
+>>> type(a)
+<class 'int'>
+>>> a=10.2
+>>> type(a)
+<class 'float'>
+>>> a=10+10j
+>>> type(a)
+<class 'complex'>
+>>> a=True
+>>> type(a)
+<class 'bool'>
+>>> int()
+0
+>>> float()
+0.0
+>>> bool()
+False
+>>> complex()
+0j
+>>>
